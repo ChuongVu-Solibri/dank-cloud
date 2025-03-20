ac382789cab4c66a17f70803c9ed11da7df42aef
ChuongVu-Solibri <cv.work.546@gmail.com>
2025-03-20 15:39:43 +0200

    Add some MUI template

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+import '@fontsource/roboto/300.css'; // Light
+import '@fontsource/roboto/400.css'; // Regular
+import '@fontsource/roboto/500.css'; // Medium
+import '@fontsource/roboto/700.css'; // Bold
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
