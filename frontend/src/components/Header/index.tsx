bd295d8238e1a3ba7202d4429de3d9dd6627752d
ChuongVu-Solibri <cv.work.546@gmail.com>
2025-03-27 22:16:49 +0200

    Rename Header component

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,10 +1,10 @@
 import { Card } from '@mui/material'
 
-const PostBox = ({ title, mediaUrl, description }: PostProps) => {
+const Header = ({ title, mediaUrl, description }: PostProps) => {
   return (
     <Card sx={{ maxWidth: 600, mb: 4 }}>
     </Card>
   );
 };
 
-export default PostBox;
+export default Header;
