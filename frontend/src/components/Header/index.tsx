bb4db71d75e7ed3fc708dc9461a7a60e95f72b00
ChuongVu-Solibri <cv.work.546@gmail.com>
2025-03-27 22:16:22 +0200

    Add Header component

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import { Card } from '@mui/material'
+
+const PostBox = ({ title, mediaUrl, description }: PostProps) => {
+  return (
+    <Card sx={{ maxWidth: 600, mb: 4 }}>
+    </Card>
+  );
+};
+
+export default PostBox;
