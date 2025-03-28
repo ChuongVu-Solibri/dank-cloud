71d99123308aafcab8f5fb0b16882ab3a8ab33ca
ChuongVu-Solibri <cv.work.546@gmail.com>
2025-03-28 13:59:06 +0200

    Add simple image view for content View

---
+++ frontend/src/components/ContentView/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+const ContentView = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <img
+        src="https://picsum.photos/800/600"
+        alt="Sample"
+        style={{ width: '100%', borderRadius: '8px' }}
+      />
+    </div>
+  )
+}
+
+export default ContentView
