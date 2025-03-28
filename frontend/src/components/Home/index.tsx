cba0f5157ca2cf729cb998485cb5fcda5cb9ba56
ChuongVu-Solibri <cv.work.546@gmail.com>
2025-03-28 14:31:13 +0200

    Add simple page for home

--- frontend/src/components/Home/index.tsx
+++ frontend/src/components/Home/index.tsx
@@ -1,25 +1,11 @@
-import PostBox from '@/components/PostBox'
+import ContentView from '@/components/ContentView'
 import DankDrawer from '@/components/DankDrawer'
 
 const Home = () => {
   return (
     <div>
       <DankDrawer />
-      <PostBox
-        title="Title 1"
-        mediaUrl="https://picsum.photos/800/600"
-        description="Description 1"
-      />
-      <PostBox
-        title="Title 2"
-        mediaUrl="https://picsum.photos/800/600"
-        description="Description 2"
-      />
-      <PostBox
-        title="Title 3"
-        mediaUrl="https://picsum.photos/800/600"
-        description="Description 3"
-      />
+      <ContentView />
     </div>
   )
 }
